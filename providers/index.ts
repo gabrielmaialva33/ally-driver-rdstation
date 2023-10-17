ca27a5302d58fc507f8d0d297fe3d85009f767d9
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-10-17 13:24:53 -0300

    chore: package.json

--- providers/index.ts
+++ providers/index.ts
@@ -7,7 +7,7 @@ export default class RDStationProvider {
     const Ally = this.app.container.resolveBinding('Adonis/Addons/Ally')
     const { RDStationDriver } = await import('../src/RDStation')
 
-    Ally.extend('rdstation', (_, __, config, ctx) => {
+    Ally.extend('RDStation', (_, __, config, ctx) => {
       return new RDStationDriver(ctx, config)
     })
   }
