0c053a2d8d49ed0455c32791a2fc2c0d5fae15ae
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-10-18 09:59:48 -0300

    refactor: rdstation routes

--- src/RDStation/index.ts
+++ src/RDStation/index.ts
@@ -104,7 +104,7 @@ export class RDStationDriver extends Oauth2Driver<RDStationAccessToken, RDStatio
    * approach is to prefix the oauth provider name to `oauth_state` value. For example:
    * For example: "facebook_oauth_state"
    */
-  protected stateCookieName = 'rdstation_oauth_state'
+  protected stateCookieName = 'rd_oauth_state'
 
   /**
    * Parameter name to be used for sending and receiving the state from.
@@ -145,7 +145,7 @@ export class RDStationDriver extends Oauth2Driver<RDStationAccessToken, RDStatio
    * hook to pre-configure the request.
    */
   protected configureRedirectRequest(request: RedirectRequest<`RDStation`>) {
-    request.clear()
+    request.scopes(['user'])
   }
 
   /**
@@ -154,7 +154,14 @@ export class RDStationDriver extends Oauth2Driver<RDStationAccessToken, RDStatio
    * the request
    */
   protected configureAccessTokenRequest(request: ApiRequest) {
-    request.clear()
+    /**
+     * Send state to rd when request is not stateles
+     */
+    if (!this.isStateless) {
+      request.field('state', this.stateCookieValue)
+    }
+
+    request.clearField('grant_type')
   }
 
   /**
@@ -163,6 +170,14 @@ export class RDStationDriver extends Oauth2Driver<RDStationAccessToken, RDStatio
    */
   public accessDenied() {
     return this.ctx.request.input('error') === 'user_denied'
+  }
+
+  protected getAuthenticatedRequest(url: string, token: string) {
+    const request = this.httpClient(url)
+    request.header('Authorization', `token ${token}`)
+    request.header('Accept', 'application/json')
+    request.parseAs('json')
+    return request
   }
 
   /**
